c89a6008cbd308b667c694b832699ce2170446fb
Abhishek Gujjar <70919722+AbhishekGujjar17@users.noreply.github.com>
2022-06-27 18:08:18 -0230

    remove link,logo,react from app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import react, { useState } from 'react';
+import { useState } from 'react';
 import Alert from './components/Alert';
 import About from './components/About';
 
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   const [mode, setMode] = useState('light');
